3765c0c6332245f254affae41fad5f48f4134b0f
Ragil14 <rahil.gilani@gmail.com>
2022-03-28 18:36:41 -0500

    add About & Contact sections to Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function Nav() {
     return (
-        <header></header>
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">About me</a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
   
