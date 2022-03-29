95800d7570eb08e68380fddff451c73bc27e1924
Ragil14 <rahil.gilani@gmail.com>
2022-03-29 16:26:19 -0500

    Call cleanup function

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
+
+afterEach(cleanup);
