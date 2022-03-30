f7ef34e7684e760259405319850ca913c3955711
Ragil14 <rahil.gilani@gmail.com>
2022-03-30 15:32:59 -0500

    add Gallery function & export it

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,3 +2,14 @@ import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
+function Gallery() {
+    return (
+      <section>
+        <h1>NAME</h1>
+        <p>DESCRIPTION</p>
+      </section>
+    );
+}
+
+export default Gallery;
+
