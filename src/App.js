e88448a412686ed066664428b055c8a1e1bfbf5b
Ragil14 <rahil.gilani@gmail.com>
2022-03-30 16:57:48 -0500

    clean code

--- src/App.js
+++ src/App.js
@@ -9,9 +9,18 @@ function App() {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { 
+      name: 'portraits', 
+      description: 'Portraits of people in my life' 
+    },
+    { 
+      name: 'food', 
+      description: 'Delicious delicacies' 
+    },
+    { 
+      name: 'landscape', 
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature' 
+    },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
