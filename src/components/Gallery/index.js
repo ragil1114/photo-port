84398927b0f75f07bf071ae02a31c62b23909b4d
Ragil14 <rahil.gilani@gmail.com>
2022-03-30 15:30:55 -0500

    update imported packages to Gallery Component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Gallery from "./components/Gallery";
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
