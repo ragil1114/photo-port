3f267849972be70aac74d21e5fb276dd343b43a0
Ragil14 <rahil.gilani@gmail.com>
2022-03-30 15:38:53 -0500

    declare a new object called currentCategory and set its name and description to values for the Commercial page

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 //import PhotoList from '../PhotoList';
 //import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Gallery() {
+function Gallery(props) {
+    const currentCategory = {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    };
     return (
       <section>
-        <h1>NAME</h1>
-        <p>DESCRIPTION</p>
+        <h1>{currentCategory.name}</h1>
+        <p>{currentCategory.name}</p>
       </section>
     );
-}
+  }
 
 export default Gallery;
 
