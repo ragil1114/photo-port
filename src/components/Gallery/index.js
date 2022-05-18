a5e74934d8629dcf469d70b01a6b268bd414007b
Ragil14 <rahil.gilani@gmail.com>
2022-05-18 18:00:00 -0500

    add notes to Gallery Component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,29 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
+
+function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <div>
+          <img
+            src={photo}
+            alt="Commercial Example"
+            className="img-thumbnail mx-1"
+          />
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
+
+
+/*
+import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
@@ -14,4 +39,5 @@ function Gallery(props) {
 }
 
 export default Gallery;
+*/
 
