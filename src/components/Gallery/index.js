e396f7cb0d62b3d5d4e735bf18ec6419f3a46c4f
Ragil14 <rahil.gilani@gmail.com>
2022-06-15 15:27:40 -0500

    delete comment

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -21,23 +21,3 @@ function Gallery(props) {
 
 export default Gallery;
 
-
-/*
-import React from 'react';
-import PhotoList from '../PhotoList';
-import { capitalizeFirstLetter } from '../../utils/helpers';
-
-function Gallery(props) {
-  const { currentCategory } = props;
-  return (
-    <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.description}</p>
-      <PhotoList category={currentCategory.name} />
-    </section>
-  );
-}
-
-export default Gallery;
-*/
-
